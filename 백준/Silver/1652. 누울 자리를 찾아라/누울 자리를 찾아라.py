76e769c34c8a9bdb89e36cbfa9c5f4ffc1c2f666
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-31 16:29:23 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -8,20 +8,20 @@ horsum = 0
 versum = 0
 
 for i in range(N):
-        hor = 0
-        ver = 0
-        for j in range(N):
-            if list1[i][j] == '.':
-                    hor += 1
-            elif list1[i][j] == 'X':
-                    hor = 0
-            if hor == 2:
-                    horsum += 1
-            if list1[j][i] == '.':
-                    ver += 1
-            elif list1[j][i] == 'X':
-                    ver = 0
-            if ver == 2:
-                    versum += 1
+    hor = 0
+    ver = 0
+    for j in range(N):
+        if list1[i][j] == '.':
+            hor += 1
+        elif list1[i][j] == 'X':
+            hor = 0
+        if hor == 2:
+            horsum += 1
+        if list1[j][i] == '.':
+            ver += 1
+        elif list1[j][i] == 'X':
+            ver = 0
+        if ver == 2:
+            versum += 1
 
 print(horsum,versum)
