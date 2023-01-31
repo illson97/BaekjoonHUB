fbfee525d2a368d01b8433b8bca9667e9447bf99
illson97 <121412131+illson97@users.noreply.github.com>
2023-01-31 16:27:46 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -0,0 +1,27 @@
+N = int(input())
+list1 = []
+
+for i in range(N):
+    list1.append(input())
+
+horsum = 0
+versum = 0
+
+for i in range(N):
+        hor = 0
+        ver = 0
+        for j in range(N):
+            if list1[i][j] == '.':
+                    hor += 1
+            elif list1[i][j] == 'X':
+                    hor = 0
+            if hor == 2:
+                    horsum += 1
+            if list1[j][i] == '.':
+                    ver += 1
+            elif list1[j][i] == 'X':
+                    ver = 0
+            if ver == 2:
+                    versum += 1
+
+print(horsum,versum)
